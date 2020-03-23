e6c86b5b9a080d922a41bce3c92658d54b4f3149
Franziska von Bethmann <franziska.bethmann@gmail.com>
2020-03-23 16:46:08 +0100

    add seed colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+colors = Color.create([
+  { name: 'red'},
+  { name: 'blue'},
+  { name: 'yellow'},
+  { name: 'green'},
+  { name: 'orange'},
+  { name: 'violet'},
+  { name: 'black'},
+  { name: 'white'}
+])
